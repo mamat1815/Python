0dfa3cceac7bc1dde3afb35e7d881a5f3bad2ff7
mamat1815 <mbahgendengngegame@gmail.com>
2024-03-06 01:15:06 +0700

    new file:   LK10

---
+++ LK11(Debugging)/cobaDebugA.py
@@ -0,0 +1,3 @@
+NamaPengguna = "Muhammad Afsar Tambawang"
+Label = "Selamat Datang, " + NamaPengguna
+print(Label)
---
+++ LK11(Debugging)/cobaDebugB.py
@@ -0,0 +1,6 @@
+angka = [1, 4, 5, 10, 31, 7, 9, 23, 14]
+sum = 0
+for i in angka:
+   sum = sum + i
+print("Jumlah semua angka adalah: ")
+print(sum)
